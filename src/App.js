14228363c95d2869212fd81c7242c53d09213b11
OlgaShelkova <olgashelkova1@gmail.com>
2024-02-24 22:29:34 +0300

    add My Daily Activities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 3],
+  ["Eat", 2],
+  ["Rest", 4],
+  ["Study", 8],
+  ["Sleep", 8],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+    chartType="PieChart"
+    data={data}
+    options={options}
+    width={"100%"}
+    height={"550px"}
+/>
     </div>
   );
 }
